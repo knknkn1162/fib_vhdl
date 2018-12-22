1e95d8985aac7ebfb5b853f1257b3e384d5a83bd
knknkn1162 <knknkn1162@gmail.com>
2018-12-22 18:42:16 +0900

    fix according to quartus prime

--- calc.vhdl
+++ calc.vhdl
@@ -14,11 +14,11 @@ end entity;
 architecture behavior of calc is
 begin
   process(clk, rst)
-    variable a, b, c : natural range 0 to 2**N-1;
+    -- NOTE: https://www.intel.com/content/www/us/en/programmable/support/support-resources/knowledge-base/solutions/rd01072011_91.html
+    variable a : natural range 0 to 2**N-1 := 1;
+    variable b, c : natural range 0 to 2**N-1 := 0;
   begin
-    if rst = '1' then
-      a := 1; b := 0; c := 0;
-    elsif rising_edge(clk) then
+    if rising_edge(clk) then
       if en = '1' then
         if a+b < 2**N-1 then
           c := b;
